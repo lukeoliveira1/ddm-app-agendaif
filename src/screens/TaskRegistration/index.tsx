2ab46ec8831e0134b21c4a46bec9e36f26b04e80
Lucas <lucasoliveirars22@gmail.com>
2024-07-11 21:39:41 -0300

    removendo comentário

--- src/screens/TaskRegistration/index.tsx
+++ src/screens/TaskRegistration/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components/native";
 
-// Definindo componentes estilizados
 const Container = styled.View`
   flex: 1;
   justify-content: center;
